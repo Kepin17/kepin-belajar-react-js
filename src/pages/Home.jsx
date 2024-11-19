a365b49832d18061b5b90ecd47d252af237c96f1
Kepin17 <servicenya4@gmail.com>
2024-11-19 16:04:36 +0700

    belajar usestate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Article from "../components/Article";
 import post from "../posts.json";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
   return (
     <div>
       <h1>Simple Blog</h1>
+
+      <div>
+        cari article : <input type="text" placeholder="cari article" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <p>ditemukan 0 untuk pencarian {search}</p>
+      </div>
       <br />
 
-      {post.map((item) => (
-        <Article title={item.title} slug={item.slug} tag={item.tags.join(",")} author={item.author} date={item.date} key={item.slug} />
+      {post.map(({ title, slug, tags, author, date }) => (
+        <Article {...{ title, slug, tags, author, date }} key={title} />
       ))}
     </div>
   );
